563def6da12d647041edb755d258f29c867b0784
Milos Milicevic <126508847+MilosM1998@users.noreply.github.com>
2025-01-04 18:10:14 +0000

    started again

---
+++ push_swap.c
@@ -0,0 +1,9 @@
+#include "push_swap.h"
+
+int main(int ac, char **av)
+{
+    if(ac == 1)
+        return (1);
+    if(ac == 2)
+    return 0;
+}
---
+++ push_swap.h
@@ -0,0 +1,9 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <limits.h>
+
+#endif
